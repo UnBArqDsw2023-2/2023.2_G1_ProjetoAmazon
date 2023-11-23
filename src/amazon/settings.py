4f2fb5bf3e9a975e3bba721bc5524d9546600cf4
Guilherme Puida <guilherme@puida.xyz>
2023-11-23 18:32:14 -0300

    move templates to base dir

--- src/amazon/settings.py
+++ src/amazon/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'amazon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
