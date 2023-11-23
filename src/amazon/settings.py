c18277027a9fc47f225cbe6f1cdb93285f327aa7
Guilherme Puida <guilherme@puida.xyz>
2023-11-23 18:40:29 -0300

    redirect to home after login

--- src/amazon/settings.py
+++ src/amazon/settings.py
@@ -125,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
