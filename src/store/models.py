688ea7a853d52b9916fb43a47566df49610e55f5
Kauã Vinícius <kauaponte@hotmail.com>
2023-11-23 20:55:09 -0300

    Adição das models , css e html

--- src/store/models.py
+++ src/store/models.py
@@ -1,5 +1,8 @@
+from typing import Any
 from django.db import models
-
+from django.utils import timezone
+from abc import ABC, abstractmethod
+from authuser.models import User
 
 class Store(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -19,3 +22,164 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class CartProxy(ABC):
+    def addProduct(self, product:Product):
+        pass
+
+    def removeProduct(self, product:Product):
+        pass
+
+    def listProducts(self):
+        pass
+
+    def getPrice(self):
+        pass
+
+class Cart(models.Model,CartProxy):
+    price = models.FloatField()
+    products: Product = []
+
+    def addProduct(self, product:Product):
+        if product not in self.products:
+            self.products.append(product)
+            self.price += product.price
+
+    def removeProduct(self, product:Product):
+        self.products.remove(product)
+
+    def listProducts(self):
+        return self.products
+
+    def getPrice(self):
+        return self.price
+
+
+class CartService(models.Model,CartProxy):
+
+    realCart:Cart
+
+    def __init__(self,realCart:Cart):
+        self.realCart = realCart
+
+    def checkLogin(self):
+        return 1
+
+    def addProduct(self, product:Product):
+        if(self.checkLogin()):
+            self.realCart.addProduct(product)
+
+    def removeProduct(self, product:Product):
+        if(self.checkLogin()):
+            self.realCart.removeProduct(product)
+
+    def listProducts(self):
+        if(self.checkLogin()):
+            return self.realCart.listProducts()
+
+    def getPrice(self):
+        if(self.checkLogin()):
+            return self.realCart.getPrice()
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True,editable=False)
+    orderDate = models.DateField()
+    status = models.CharField(max_length=250)
+    cart:CartService
+    account: User
+    value = models.FloatField()
+
+    def __init__(self):
+        pass
+
+    def describeOrder(self):
+        pass
+
+    def generatePayment(self):
+        payment = Payment()
+
+
+class PaymentMode(ABC):
+    @abstractmethod
+    def pay(self,data):
+        pass
+
+    @abstractmethod
+    def generate(self,data):
+        pass
+
+class Payment(models.Model):
+    paymentId = models.UUIDField(primary_key=True,editable=False)
+    value = models.FloatField()
+    actualData = models.DateField(default=timezone.now())
+    expirationData = models.DateField()
+    status = models.TextField(blank=True,default="")
+    costOfFreight = models.FloatField()
+    coupon = models.BooleanField()
+    discount = models.FloatField()
+    finalValue = models.FloatField()
+    paymentMode:PaymentMode = models.Model 
+
+    def __init__(self,costOfFreight,coupon,discount,value):
+        self.costOfFreight = costOfFreight
+        self.coupon = coupon
+        self.discount = discount
+        self.value = value 
+
+    def __str__(self):
+        return self.paymentId
+
+    def setPaymentMode(self,paymentMode):
+        self.paymentMode = paymentMode   
+
+    def payValue(self):
+        self.paymentMode.pay()
+    
+    def generatePayment(self):
+        self.paymentMode.generate()
+    
+    def verifyStatus(self):
+        return self.status
+
+    def sumCostOfFreight(self):
+        return self.costOfFreight
+
+    def finish(self):
+        pass    
+
+class Pix(models.Model,PaymentMode):
+    chavePix = models.CharField(max_length=250)
+
+    def pay(self,data):
+        pass
+
+    def generate(self,data):
+        pass
+
+class Ticket(models.Model,PaymentMode):
+    barCode = models.CharField(max_length=250)
+    recipientName = models.CharField(max_length=250)
+    recipientCPF = models.CharField(max_length=14)
+
+    def __init__(self,recipientName,recipientCPF):
+        self.recipientCPF = recipientCPF
+        self.recipientName = recipientName
+
+    def pay(self,data):
+        pass
+
+    def generate(self,data):
+        pass
+
+class CreditCard(models.Model,PaymentMode):
+    cardName = models.CharField(max_length=250)
+    number = models.CharField(max_length=16)
+    cvv = models.CharField(max_length=3)
+    expirationDate = models.CharField(max_length=10)
+
+    def pay(self,data):
+        pass
+
+    def generate(self,data):
+        pass
+
--- src/store/views.py
+++ src/store/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def payment(request):
+    return render(request, 'payment.html')
+
+def product(request):
+    return render(request, 'product.html')
