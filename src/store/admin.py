e25bc4d34e072a880ce51e9019bf0d0b9f27b264
Guilherme Puida <guilherme@puida.xyz>
2023-11-23 18:13:42 -0300

    Store and Product models

--- src/store/admin.py
+++ src/store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Store, Product
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'amount_in_stock', 'sold_by']
--- src/store/models.py
+++ src/store/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Store(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, default="")
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, default="")
+    sold_by = models.ForeignKey(Store, on_delete=models.CASCADE)
+
+    price = models.FloatField()
+    amount_in_stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
