d93a2a1edc0636d9900945dc04fa877d4173220b
Guilherme Puida <guilherme@puida.xyz>
2023-11-23 19:25:21 -0300

    profile screen

--- src/authuser/urls.py
+++ src/authuser/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
 
+from authuser.views import profile
+
 urlpatterns = [
-    path('', include('django.contrib.auth.urls'))
+    path('', include('django.contrib.auth.urls')),
+    path('profile', profile, name='profile'),
 ]
--- src/authuser/views.py
+++ src/authuser/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from authuser.models import User
+
+@login_required
+def profile(request):
+    user = User.objects.get(id=request.user.id)
+    return render(request, 'registration/profile.html', {'user': user})
