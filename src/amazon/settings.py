bd20dceadf0ccd20e27fe0f88b5d438aab25e8ab
Guilherme Puida <guilherme@puida.xyz>
2023-11-23 17:54:35 -0300

    configure custom user model as authuser

--- src/amazon/settings.py
+++ src/amazon/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'authuser',
 ]
 
+AUTH_USER_MODEL = 'authuser.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
