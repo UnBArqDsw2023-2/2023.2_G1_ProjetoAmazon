4fd27d820adadfcf20a853866bdd1593d241fc35
Guilherme Puida <guilherme@puida.xyz>
2023-11-23 18:15:22 -0300

    register User model in admin

--- src/authuser/admin.py
+++ src/authuser/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from authuser.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'name', 'is_active', 'is_staff', 'is_superuser', 'last_login', 'date_joined']
